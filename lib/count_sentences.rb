d66e0cf2b688b1a9e1b266e72006dfc7dc9d287e
Andrew Och <andrewoch95@gmail.com>
2018-07-18 17:20:25 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+    new_string = self.split(/[?!.]/)
+    new_string.delete_if do |sentence|
+      sentence == ""
+    end
+    return new_string.length
   end
 end
