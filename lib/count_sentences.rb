f9d9e5cd0309527499fa79b9f0cf9eca7e4295fe
TylerNodell <Tyler.l.nodell@gmail.com>
2018-07-18 17:24:55 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    end_with?('.')
   end
 
   def question?
-
+    end_with?('?')
   end
 
   def exclamation?
-
+    end_with?('!')
   end
 
   def count_sentences
-
+    sentences = split(/\.|\?|\!/)
+    sentences.map { |word|
+      word.length <= 1 ? sentences.delete(word) : word
+    }.count
   end
 end
